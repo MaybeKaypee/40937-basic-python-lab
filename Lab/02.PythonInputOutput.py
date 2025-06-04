8c72ea3bb0c72e8adf5cd2cbfb897f4ee3de42fd
Kumpun <kpkiettipong@gmail.com>
2025-06-04 10:02:08 +0700

    a

---
+++ Lab/02.PythonInputOutput.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+like = input("What do you like? ")
+print(name + " likes " + like + ".")
+
---
+++ Lab/03.PythonVariables.py
@@ -0,0 +1,5 @@
+team1 = ("Liverpool")
+team2 = ("Chelsea")
+score1 = 4
+score2 = 3
+print(f"{team1} versus {team2}\n{score1} to {score2}")
